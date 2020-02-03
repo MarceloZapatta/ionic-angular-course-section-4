d975468d8cb25a232f5d8afa17df8e304ca82f3b
Marcelo Zapatta <marcelo@olivasdigital.com.br>
2020-02-03 11:24:52 -0300

    Add Angular Components in Ionic prj

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -9,13 +9,13 @@ export class RecipesService {
     {
       id: 'r1',
       title: 'Schnitzel',
-      imageUrl: 'https://shorturl.at/hnDG7',
+      imageUrl: 'https://www.thespruceeats.com/thmb/VYxi1MPOLh3W286dhp270ozcjrY=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
       ingrediends: ['French Frie', 'Pork Meat', 'Salad']
     },
     {
       id: 'r2',
       title: 'Spaghetti',
-      imageUrl: 'https://shorturl.at/klJT1',
+      imageUrl: 'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
       ingrediends: ['Spaghetti', 'Tomatoe Sauce', 'Chesse']
     }
   ];
@@ -28,5 +28,11 @@ export class RecipesService {
 
   getRecipe(id: string) {
     return {...this.recipes.find(recipe => recipe.id === id)};
+  }
+
+  deleteRecipe(id: string) {
+    this.recipes = this.recipes.filter(recipe => {
+      return recipe.id !== id;
+    });
   }
 }
